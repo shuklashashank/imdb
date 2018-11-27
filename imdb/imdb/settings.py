682aec5286033b998dc1d0fd73a40b100ee1ed65
shashank shukla <shashank.shukla@easygov.co.in>
2018-11-28 03:44:16 +0530

    no message

--- imdb/imdb/settings.py
+++ imdb/imdb/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'whitenoise.runserver_nostatic'
-    'imdb.imdbapp',
+    'imdbapp',
+# ,    'imdb.imdbapp',
 ]
 
 MIDDLEWARE = [
